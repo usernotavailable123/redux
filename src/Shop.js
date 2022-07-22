60d971434efe6cf7fca99505ce058b5df99d9470
usernotavailable123 <102803913+usernotavailable123@users.noreply.github.com>
2022-07-22 23:42:57 +0800

    create a action-creators & reducers

--- src/Shop.js
+++ src/Shop.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Shop = () => {
   return (
     <div>
-        <h2>Buy adidas Shoes 50 Rs</h2>
+        <h2> Deposit/Withdraw Money</h2>
       <button className="btn btn-primary mx-2">+</button>
       Add this item to cart
       <button className="btn btn-primary mx-2">-</button>
